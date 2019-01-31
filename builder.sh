5a6de68d63f9c90a6db98fb8a9de23aa5163dabf
singhvkrm <singh.vkrm@gmail.com>
2019-01-31 10:48:03 +0100

    Create builder.sh

---
+++ builder.sh
@@ -0,0 +1,9 @@
+docker pull registry.redhat.io/jboss-webserver-3/webserver31-tomcat7-openshift:v3.11.16 \
+docker pull registry.redhat.io/jboss-webserver-3/webserver31-tomcat8-openshift:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-2-rhel7:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-agent-maven-35-rhel7:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-agent-nodejs-8-rhel7:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-slave-base-rhel7:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-slave-maven-rhel7:v3.11.16 \
+docker pull registry.redhat.io/openshift3/jenkins-slave-nodejs-rhel7:v3.11.16
+echo "Done"
